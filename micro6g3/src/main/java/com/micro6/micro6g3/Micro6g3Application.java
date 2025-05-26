2ddff93b0fc2ec9a6b1b5a2136bbc84f1d6e2874
Maicol Antonio Saldivia Silva <mai.saldivia@duocuc.cl>
2025-05-26 03:55:01 -0400

    Primer commit del microservicio de reseña y calificación

---
+++ micro6g3/src/main/java/com/micro6/micro6g3/Micro6g3Application.java
@@ -0,0 +1,13 @@
+package com.micro6.micro6g3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Micro6g3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Micro6g3Application.class, args);
+	}
+
+}
---
+++ micro6g3/src/test/java/com/micro6/micro6g3/Micro6g3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro6.micro6g3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Micro6g3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
